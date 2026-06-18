60c2125f3449245d1b54fdba29958ac46cb98213
Ravi Zare <ravizare.zare@gmail.com>
2026-06-18 21:44:56 +0530

    Configured project-level token retrieval view endpoint

--- reservation/urls.py
+++ reservation/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Clean paths for your generic menu views
     path('menu/items', views.MenuItemsView.as_view(), name='menu_items'),
     path('menu/items/<int:pk>', views.SingleMenuItemView.as_view(), name='single_menu_item'),
-    
-    # Add this line to wire up your secured endpoint:
     path('msg/', views.securedview, name='secured_msg'), 
 ]
