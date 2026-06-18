5afcff89532b0debd08de6d8344547aa74f070e6
Ravi Zare <ravizare.zare@gmail.com>
2026-06-18 21:10:38 +0530

    Added UserViewSet with IsAuthenticated permission controls

--- reservation/views.py
+++ reservation/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, permissions  # 1. Added 'permissions' here
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth.models import User
@@ -6,11 +6,13 @@ from .models import MenuItem, Booking
 from .serializers import UserSerializer, BookingSerializer, MenuItemSerializer  
 
 # --- 1. User ViewSet for the DefaultRouter ---
+# Added right here at the top of your view classes
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+   queryset = User.objects.all() 
+   serializer_class = UserSerializer
+   permission_classes = [permissions.IsAuthenticated] # Restricts access to logged-in users only
 
-# --- 2. Step 3: Booking ViewSet (Replaced the old BookingView) ---
+# --- 2. Booking ViewSet for the Tables Router ---
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
