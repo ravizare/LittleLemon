ceb1892dac3072f590c22fee70d15867ede19554
Ravi Zare <ravizare.zare@gmail.com>
2026-06-30 15:26:47 +0530

    Convert littlelemon from submodule to normal directory

---
+++ littlelemon/urls.py
@@ -0,0 +1,41 @@
+"""
+URL configuration for littlelemon project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
+from restaurant import views 
+
+router = routers.DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    # Keeps your booking tables endpoints cleanly grouped
+    path('restaurant/booking/', include(router.urls)),
+    
+    # Wires up your app's URLs to follow the required /api/ structure
+    path('api/', include('restaurant.urls')), 
+    
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-token-auth/', obtain_auth_token), 
+    
+    # 👇 ADDED DJOSER AUTHENTICATION ENDPOINTS HERE 👇
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+]
